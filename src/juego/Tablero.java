d845378e8f6a156683b577fd50aa40fefa229a17
coloco19 <81964683+coloco19@users.noreply.github.com>
2021-05-20 12:11:57 -0300

    Update Tablero.java

--- src/juego/Tablero.java
+++ src/juego/Tablero.java
@@ -7,8 +7,9 @@ public class Tablero {
 		public int contarPuntos() {
 			return 1;
 		}
-		public boolean puedeInsertar(int posX, int posY, Terreno terreno) {
-			return tablero[posX][posY].compararTerreno(terreno);
+		public boolean puedeInsertar(int posX, int posY, Terreno terreno, int[] direccion) {
+			return (!fueraDeTablero(posX, posY, direccion) && compararTerrenoAledanio(posX, posY, terreno))
+				&& (hayEspacioAledanio(posX, posY));
 		}
 		
 		public void insertarFicha(Ficha ficha, int posX, int posY, int[] direccion) {
@@ -19,5 +20,23 @@ public class Tablero {
 		}
 		public void actualizarTablero() {
 			return;
+		}
+		public boolean esBorde(int posX, int posY) {
+		return posX == 0 || posY == 0 || posX == this.tablero.length || posY == this.tablero[0].length;
+		}
+
+		private boolean compararTerrenoAledanio(int posX, int posY, Terreno terreno) {
+			return terreno.compararTerreno(tablero[posX + 1][posY]) || terreno.compararTerreno(tablero[posX - 1][posY])
+					|| terreno.compararTerreno(tablero[posX][posY + 1]) || terreno.compararTerreno(tablero[posX][posY - 1]);
+		}
+
+		private boolean fueraDeTablero(int posX, int posY, int[] direccion) {
+			return posX + direccion[0] < 0 || posY + direccion[1] < 0 || posX + direccion[0] == this.tablero.length
+					|| posX + direccion[0] == this.tablero.length;
+		}
+
+		private boolean hayEspacioAledanio(int posX, int posY) {
+			return tablero[posX][posY - 1] == null || tablero[posX - 1][posY] == null || tablero[posX][posY + 1] == null
+					|| tablero[posX + 1][posY] == null;
 		}
 }
