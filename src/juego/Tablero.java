69db991654e31c9fd9b97df7f24df76d69eb614c
coloco19 <81964683+coloco19@users.noreply.github.com>
2021-05-19 21:21:12 -0300

    Update Tablero.java

--- src/juego/Tablero.java
+++ src/juego/Tablero.java
@@ -7,15 +7,16 @@ public class Tablero {
 		public int contarPuntos() {
 			return 1;
 		}
-		
-		public boolean puedeInsertar() {
-			return true;
+		public boolean puedeInsertar(int posX, int posY, Terreno terreno) {
+			return tablero[posX][posY].compararTerreno(terreno);
 		}
 		
-		public void insertarFicha(Ficha ficha) {
-			return;
+		public void insertarFicha(Ficha ficha, int posX, int posY, int[] direccion) {
+			tablero[posX][posY] = ficha.getTerreno1();
+			tablero[posX-direccion[0]][posY-direccion[1]] = ficha.getTerreno2();
+			//pasamos la ficha a insertar, la posicion donde la vamos a ubicar y un vector de int que
+			//tiene en que posicion, si va hacia la izquierda los valores del vector serian -1 y 0
 		}
-		
 		public void actualizarTablero() {
 			return;
 		}
